d7619a48257f4d7cb443e36d12d55a2a107c4dfe
igor <ufolog1368@gmail.com>
2020-08-27 20:01:40 +0300

    create test Circle getRadius

---
+++ src/test/java/by/komarovigor/bean/CircleTest.java
@@ -0,0 +1,23 @@
+package by.komarovigor.bean;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class CircleTest {
+
+    @Test
+    public void getRadius() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+
+    @Test
+    public void testHashCode() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
