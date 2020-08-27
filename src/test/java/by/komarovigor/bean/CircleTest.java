d4e0b2e933700628cb1194cc5a4bfcbcae15d534
igor <ufolog1368@gmail.com>
2020-08-27 20:13:14 +0300

    create test Circle Equals

--- src/test/java/by/komarovigor/bean/CircleTest.java
+++ src/test/java/by/komarovigor/bean/CircleTest.java
@@ -3,15 +3,46 @@ package by.komarovigor.bean;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
 public class CircleTest {
 
     @Test
     public void getRadius() {
+        var circle = new Circle();
+        circle.setRadius(40.5);
+        double expected = 40.5;
+        double actual = circle.getRadius();
+        assertEquals(expected, actual, 0);
     }
 
     @Test
-    public void testEquals() {
+    public void testEqualsTrue() {
+        var circle1 = new Circle();
+        circle1.setRadius(40.5);
+
+        var circle2 = new Circle();
+        circle2.setRadius(40.5);
+
+        boolean expected = true;
+        boolean actual = circle1.equals(circle2);
+        assertEquals(expected, actual);
+
     }
+
+    @Test
+    public void testEqualsFalse() {
+        var circle1 = new Circle();
+        circle1.setRadius(40.5);
+
+        var circle2 = new Circle();
+        circle2.setRadius(40.7);
+
+        boolean expected = false;
+        boolean actual = circle1.equals(circle2);
+        assertEquals(expected, actual);
+
+    }
+
 
     @Test
     public void testHashCode() {
